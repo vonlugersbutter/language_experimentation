18318e3d85166fc4d43daf21b59c4842c5f0bdc3
Ayesha Umair <ayeshaumair10@gmail.com>
2019-01-27 15:19:58 -0500

    Create java_practice.java
    Just some superficial practice

---
+++ java_practice.java
@@ -0,0 +1,18 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+
+    int a_number = 5;
+    int another_number = 4;
+
+    int last_number;
+    last_number = a_number + another_number;
+
+    System.out.println(last_number);
+
+    char c = 'a';
+
+    System.out.printIn(char);
+
+  }
+}
