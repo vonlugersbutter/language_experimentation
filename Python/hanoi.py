77200cb177f0c3154a4679dd326b6f42cd3d5e91
Ayesha Umair <ayeshaumair10@gmail.com>
2019-08-14 17:23:44 -0400

    Create hanoi.py
    not done

---
+++ Python/hanoi.py
@@ -0,0 +1,33 @@
+# tower of hanoi
+
+for num in range(-1,-(len(i)+1),-1):
+    print(i[num])
+print()
+
+class tower(list):
+    def __init__(self,position):
+        self.position = position
+    def put(self,d):
+        self.append(d)
+    def take(self):
+        d = list.pop(self)
+        return d
+
+def tower_of_hanoi(num, )
+
+j.append(i.pop())
+k.append(i.pop())
+k.append(j.pop())
+
+j.append(i.pop())
+j.append(k.pop())
+i.append(k.pop())
+i.append(j.pop())
+k.append(j.pop())
+j.append(i.pop())
+k.append(i.pop())
+k.append(j.pop())
+
+for num in range(-1,-(len(k)+1),-1):
+    print(k[num])
+print()
